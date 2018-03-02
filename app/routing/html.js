6319b8794c0ada09e68825ef7204a48d0d7c7a17
Kyle Cohen <spausek@gmail.com>
2018-03-02 15:50:05 -0800

    Changed the file structure for CSS. Added a static path so CSS would work. html.api now serves my 2 html file

--- app/routing/html.js
+++ app/routing/html.js
@@ -1,11 +1,11 @@
-const path = require('path');
+var path = require('path');
 
-module.export = function(app){
-	app.get('/home', (req, res) =>{
-		res.sendFile(path.join(__dirname + '/../view/home.html'));
+module.exports = function (app){
+	app.get('/survey', (req, res) =>{
+		res.sendFile(path.join(__dirname + '/../view/survey.html'));
 	});
 
-	app.get ('/survey', (req, res) => {
-		res.sendFile(path.join(__dirname + '/../view/survey.html'));
-	})
+	app.use((req, res) => {
+		res.sendFile(path.join(__dirname + '/../view/home.html'));
+	});
 }
--- server.js
+++ server.js
@@ -8,9 +8,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(bodyParser.text());
 app.use(bodyParser.raw({ type: 'application/vnd.api+json' }))
+app.use(express.static('app/view'));
+
+require('./app/routing/api.js')(app); 
+require('./app/routing/html.js')(app);
 
 
 app.listen(PORT, function (){
- console.log('Listening on port 3000');
+ console.log('Listening on port 8080');
 });
 
